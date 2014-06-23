05c232ced767fbb6f87378d5eb86f752ba9f1d1d
nanaprempeh009 <nanaprempeh009@gmail.com>
2014-06-23 15:01:30 +0000

    All updates

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
 
     map = new GMaps({
         div: '#map',
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 5.644381,
+        lng: -0.151555,
     });
     map.addMarker({
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 5.644381,
+        lng: -0.151555,
         title: 'Address',      
         infoWindow: {
-            content: '<h5 class="title">Company name</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
+            content: '<h5 class="title">Startup360</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
         }
         
     });
